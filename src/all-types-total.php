b14cf70549e04af8eb726cf0e98c0e5a81faef97
JDGrimes <jdg@codesymphony.co>
2017-10-03 17:57:46 -0400

    dev-lib: update to test against WordPoints 2.4.0-beta-1

--- src/all-types-total.php
+++ src/all-types-total.php
@@ -51,7 +51,7 @@ WordPoints_Class_Autoloader::register_dir( dirname( __FILE__ ) . '/classes/' );
  *
  * @since 1.0.0
  */
-require_once( dirname( __FILE__ ) . '/includes/functions.php' );
+require_once dirname( __FILE__ ) . '/includes/functions.php';
 
 WordPoints_Shortcodes::register(
 	'wordpoints_all_types_total'
